4c970368b17a72b25bfa74f9820c90756ed09b0e
no-int <tkdduf2122@naver.com>
2025-08-07 22:35:13 +0900

    init

---
+++ src/main/java/com/trebari/githubissues/GithubIssuesApplication.java
@@ -0,0 +1,13 @@
+package com.trebari.githubissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIssuesApplication.class, args);
+    }
+
+}
